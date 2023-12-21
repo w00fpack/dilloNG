ce9ffe712b8e65e0efd64beb9818a05438db6a5b
Axel Beckert <abe@debian.org>
2023-12-20 20:51:38 -0500

    Fix FTBFS with gcc-10

--- dpid/dpid_common.h
+++ dpid/dpid_common.h
@@ -37,10 +37,7 @@
 
 
 /*! Error codes for dpid */
-enum {
-   no_errors,
-   dpid_srs_addrinuse /* dpid service request socket address already in use */
-} dpi_errno;
+extern enum dpi_errno;
 
 /*! Intended for identifying dillo plugins
  * and related files
